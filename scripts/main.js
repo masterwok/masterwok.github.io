1b68c57c3ca54d5699d8c5e45ec3409cc2ed44f0
Jonathan Trowbridge <Jonathan.Trowbridge@gmail.com>
2016-06-21 03:21:35 -0400

    Das skeleton of contact form

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+var resumeApp = resumeApp || {};
+
 // Fix orientation scaling bug (http://stackoverflow.com/questions/2557801)
 if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
     var viewportmeta = document.querySelector('meta[name="viewport"]');
@@ -11,9 +14,13 @@ if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) 
     }
 }
 
-new WOW().init(); 
 
-new GameOfLife({
+// Start the WOW animation instance
+resumeApp.wowInstance = new WOW();
+resumeApp.wowInstance.init();
+
+// Start the game of life
+resumeApp.gameInstance = new GameOfLife({
     canvasId: 'gameOfLifeTarget'
     , cellSize: 3
     , ratioAlive: 0.12
@@ -22,4 +29,74 @@ new GameOfLife({
     , cycleColors: false
     , color: '#d3d3d3'
     , keepAlive: true
-}).run();
+});
+resumeApp.gameInstance.run();
+
+resumeApp.message = {
+    isValid: function(nameInput, emailInput, messageInput) {
+        var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var isValid = true;
+
+        // Validate name input
+        if (nameInput.value) {
+            this.removeErrorFromInput(nameInput);
+        } else {
+            isValid = false;
+            this.addErrorToInput(nameInput);
+        }
+
+        // Validate email input
+        if (emailInput.value && emailRegex.test(emailInput.value)) {
+            this.removeErrorFromInput(emailInput);
+        } else {
+            isValid = false;
+            this.addErrorToInput(emailInput);
+        }
+
+        // Validate message input
+        if (messageInput.value) {
+            this.removeErrorFromInput(messageInput);
+        } else {
+            isValid = false;
+            this.addErrorToInput(messageInput);
+        }
+
+        return isValid;
+    }
+    , addErrorToInput: function(input) {
+        if (!input.parentNode.classList.contains('form-group')) {
+            throw 'Input must be within a form-group';
+        }
+
+        input.parentNode.classList.add('has-error');
+    }
+    , removeErrorFromInput: function(input) {
+        if (!input.parentNode.classList.contains('form-group')) {
+            throw 'Input must be within a form-group';
+        }
+
+        input.parentNode.classList.remove('has-error');
+    }
+    , send: function() {
+        var form = document.getElementById('contactForm');
+        var nameInput = form.querySelectorAll('input[name="name"]')[0];
+        var emailInput = form.querySelectorAll('input[name="email"]')[0];
+        var messageInput = form.querySelectorAll('textarea[name="message"]')[0];
+
+        var successMessage = form.querySelectorAll('.form-success-message')[0];
+        var errorMessage = form.querySelectorAll('.form-error-message')[0];
+
+        if (this.isValid(nameInput, emailInput, messageInput)) {
+            errorMessage.style.display = 'none';
+            successMessage.style.display = 'inline-block';
+            console.log('sending messag...');
+
+            return false;
+        }
+
+        successMessage.style.display = 'none';
+        errorMessage.style.display = 'inline-block';
+
+        return false;
+    }
+};
