76801e9c562b8ed64d3cfcccf78cc2ef012bd73c
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2025-06-09 16:48:54 -0400

    Fix CSS loading

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -18,7 +19,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(svg|png|jpe?g|gif)$/i,
@@ -37,10 +38,13 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'public', 
-          to: '.',        
+          from: 'public',
+          to: '.',
         },
       ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
     }),
   ],
   devServer: {
