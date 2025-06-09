d2bea515f4c413d1caed832291b26b6507a318c0
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2025-06-09 16:28:01 -0400

    WIP on new resume format

--- README.md
+++
@@ -1,4 +0,0 @@
-# masterwok.github.io
-This repository contains my web resume and PDF CV.
-
-[Available Here](http://masterwok.github.io)
---
+++ webpack.config.cjs
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    liveReload: true,
+    watchFiles: ['src/**/*'],
+    static: [
+      {
+        directory: path.resolve(__dirname, 'public'),
+        publicPath: '/',
+      },
+    ],
+  },
+};
