356ab13d363db860f04118f03ba8fdd265466835
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2025-06-09 16:41:00 -0400

    Build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,14 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    path: path.resolve(__dirname),
   },
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -19,24 +19,40 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      }],
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public', 
+          to: '.',        
+        },
+      ],
     }),
   ],
   devServer: {
-    static: './dist',
-    port: 3000,
-    open: true,
-    liveReload: true,
-    watchFiles: ['src/**/*'],
     static: [
       {
         directory: path.resolve(__dirname, 'public'),
         publicPath: '/',
       },
     ],
+    port: 3000,
+    open: true,
+    liveReload: true,
+    watchFiles: ['src/**/*'],
   },
 };
