53b93411732741a2f4a10bbaf52d735ff682f31a
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2025-06-09 22:19:39 -0400

    Email and cowsay in console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './index.css';
+
+console.log(`
+ __________________________________________________
+/ You can find the repo here:                      \\
+| https://github.com/masterwok/masterwok.github.io |
+\\_________________________________________________/
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+`);
